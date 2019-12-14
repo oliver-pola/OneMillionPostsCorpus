78330899d6d15cce9cfd001cd0f204da7bce5458
Oliver Pola <5pola@informatik.uni-hamburg.de>
2019-12-14 19:32:10 +0100

    zip cleanup

--- corpus.py
+++ corpus.py
@@ -14,15 +14,16 @@ def get_conn():
     url = 'https://github.com/OFAI/million-post-corpus/releases/download/v1.0.0/million_post_corpus.tar.bz2'
     db = 'data/corpus.sqlite3'
     zip = os.path.split(url)[-1]
-    if not os.path.exists(zip):
-        print('Downloading corpus file...')
-        wget.download(url, zip)
-        print()
     if not os.path.exists(db):
+        if not os.path.exists(zip):
+            print('Downloading corpus file...')
+            wget.download(url, zip)
+            print()
         print('Extract corpus DB...')
         with tarfile.open(zip) as tar:
             tar.extractall()
         os.rename('million_post_corpus', 'data')
+        os.remove(zip)
     conn = sqlite3.connect(db)
     return conn
 
@@ -30,8 +31,8 @@ def get_conn():
 if __name__ == '__main__':
 	# Test
     all_tables = ['Articles', 'Posts', 'Newspaper_Staff', 'Annotations', 'Annotations_consolidated', 'CrossValSplit', 'Categories']
-    print('Testing DB connection...')
     conn = get_conn()
+    print('Testing DB connection...')
     cur = conn.cursor()
     for table in all_tables:
         cur.execute(f'SELECT COUNT(*) FROM {table}')
