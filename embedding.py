1017c97e933f2c3850f678988a0bd8d04e46bfe2
Oliver Pola <5pola@informatik.uni-hamburg.de>
2020-01-23 11:02:56 +0100

    gensim word2vec test

--- embedding.py
+++ embedding.py
@@ -9,31 +9,42 @@ import shutil
 import wget
 import tarfile
 import numpy as np
+import gensim
 
 import corpus # just to ensure data dir is rearranged
 
 
 def get_embedding():
     """ Word2Vec Embedding German
-        Refrence: https://deepset.ai/german-word-embeddings
+        Refrence: https://github.com/devmount/GermanWordEmbeddings
     """
-    vocab_url = 'https://int-emb-word2vec-de-wiki.s3.eu-central-1.amazonaws.com/vocab.txt'
-    vectors_url = 'https://int-emb-word2vec-de-wiki.s3.eu-central-1.amazonaws.com/vectors.txt'
-    vocab = 'data/word2vec/vocab.txt'
-    vectors = 'data/word2vec/vectors.txt'
+    # https://deepset.ai/german-word-embeddings
+    # vocab_url = 'https://int-emb-word2vec-de-wiki.s3.eu-central-1.amazonaws.com/vocab.txt'
+    # vectors_url = 'https://int-emb-word2vec-de-wiki.s3.eu-central-1.amazonaws.com/vectors.txt'
+    # vocab = 'data/word2vec/vocab.txt'
+    # vectors = 'data/word2vec/vectors.txt'
+    vectors_url = 'http://cloud.devmount.de/d2bc5672c523b086/german.model'
+    vectors = 'data/word2vec/german.model'
     corpus.get_conn() # just to ensure data dir is rearranged
     os.makedirs('data/word2vec', exist_ok=True)
-    if not os.path.exists(vocab):
-        print('Downloading Word2Vec vocabulary...')
-        wget.download(vocab_url, vocab)
-        print()
+    # if not os.path.exists(vocab):
+    #     print('Downloading Word2Vec vocabulary...')
+    #     wget.download(vocab_url, vocab)
+    #     print()
     if not os.path.exists(vectors):
         print('Downloading Word2Vec vectors...')
         wget.download(vectors_url, vectors)
         print()
-    return True
+    print('Load Word2Vec embedding...')
+    embedding = gensim.models.KeyedVectors.load_word2vec_format(vectors, binary=True)
+    return embedding
 
 
 if __name__ == '__main__':
 	# Test
-    get_embedding()
+    embedding = get_embedding()
+    test = ['Dies', 'ist', 'ein', 'Test']
+    test = ['Dies', 'Test'] # not in vocabulary: ist, ein
+    print(test)
+    embedded = [embedding[w] for w in test]
+    print(embedded)
